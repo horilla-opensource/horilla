1e9056ecfc05b77f4914e95a84cbf1252651aae6
Nikhil <nikhil@cybrosys.in>
2023-06-06 12:17:27 +0530

    [UPDT]employee updates

--- employee/admin.py
+++ employee/admin.py
@@ -1,11 +1,13 @@
+"""
+admin.py
+
+This page is used to register the model with admins site.
+"""
 from django.contrib import admin
-from employee.models import  Employee,   EmployeeWorkInformation,EmployeeBankDetails
-from simple_history.admin import SimpleHistoryAdmin
+from employee.models import Employee, EmployeeWorkInformation, EmployeeBankDetails
 
 # Register your models here.
 
 admin.site.register(Employee)
 admin.site.register(EmployeeBankDetails)
 admin.site.register(EmployeeWorkInformation)
-
-
--- employee/apps.py
+++ employee/apps.py
@@ -1,6 +1,19 @@
+"""
+apps.py
+"""
 from django.apps import AppConfig
 
 
 class EmployeeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'employee'
+    """
+    AppConfig for the 'employee' app.
+
+    This class represents the configuration for the 'employee' app. It provides
+    the necessary settings and metadata for the app.
+
+    Attributes:
+        default_auto_field (str): The default auto field to use for model field IDs.
+        name (str): The name of the app.
+    """
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "employee"
