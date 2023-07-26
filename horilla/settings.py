c6b3e3792bbc3e5e0d9bce97578afd9197874bd3
Horilla <teamhorilla@gmail.com>
2023-07-26 14:21:36 +0530

    [FIX]drag and drop in recruitment sorting issue

--- horilla/settings.py
+++ horilla/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'horilla.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'TestDB_Horilla.sqlite3',
     }
 }
 
