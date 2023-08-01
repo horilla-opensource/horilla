243803b5c406817210123b20e66c598afdf72e29
Horilla <teamhorilla@gmail.com>
2023-08-01 16:47:47 +0530

    [ADD]notifications app

---
+++ notifications/models.py
@@ -0,0 +1,15 @@
+from swapper import swappable_setting
+from django.db import models
+from .base.models import AbstractNotification, notify_handler  # noqa
+
+
+class Notification(AbstractNotification):
+    verb_en = models.CharField(max_length=255,default='',null=True)
+    verb_ar = models.CharField(max_length=255,default='',null=True)
+    verb_de = models.CharField(max_length=255,default='',null=True)
+    verb_es = models.CharField(max_length=255,default='',null=True)
+    verb_fr = models.CharField(max_length=255,default='',null=True)
+
+    class Meta(AbstractNotification.Meta):
+        abstract = False
+        swappable = swappable_setting('notifications', 'Notification')
