4d9b06a9e9c94fee223ec1603a4185fd31e60154
NIKHIL RAVI <nikhil@cybrosys.in>
2023-08-21 17:23:42 +0530

    [UPDT]payroll dashboard updates

--- payroll/static/payroll/dashboard.js
+++ payroll/static/payroll/dashboard.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){ 
-    // $('#monthYearField').datepicker({
-    //     format: 'mm/yyyy',
-    //     viewMode: 'months',
-    //     minViewMode: 'months'
-    // });
 
-
-    
     var myDate = new Date();
     var year = myDate.getFullYear();
     var month = ('0' + (myDate.getMonth())).slice(-2); // Month is zero-indexed, so add 1 and pad with leading zero if needed
@@ -38,9 +31,26 @@ $(document).ready(function(){
                 scales: {
                     x: {
                         stacked: true,
+                        title: {
+                        display: true,
+                        text: "Name of Employees",
+                        font: {
+                            weight: "bold", 
+                            size: 16,
+                          },
+                        },
                     },
                     y: {
-                        stacked: true
+                        stacked: true,
+                        title: {
+                        display: true,
+                        text: "Amount",
+                        font: {
+                            weight: "bold", 
+                            size: 16,
+                          },
+                        
+                        },
                     }
                 }
             }
@@ -96,6 +106,7 @@ $(document).ready(function(){
 
                 $("#select_employee").html("")
                 $("#select_employee").append("<option></option>")
+                
                 $.each(employees,function(key,item) {
 
                     $("#select_employee").append($('<option>', {
@@ -161,6 +172,33 @@ $(document).ready(function(){
                 type: "pie",
                 data: departmentChartData,
             });
+
+            $("#departmentChart").on("click", function(event) {
+                var activeBars = departmentPayrollChart.getElementsAtEventForMode(event, "index", { intersect: true }, true);
+        
+                if (activeBars.length > 0) {
+                    var clickedBarIndex = activeBars[0].index;
+                    var clickedLabel = departmentChartData.labels[clickedBarIndex];
+                    
+                    $.ajax({
+                        url: '/payroll/filter-payslip?department='+clickedLabel,
+                        type: "GET",
+                        dataType: "html",
+                        headers: {
+                            "X-Requested-With": "XMLHttpRequest",
+                        },
+                        success: (response) => {
+                            $("#back_button").removeClass("d-none")
+                            $("#dashboard").html(response)
+            
+            
+                        },
+                        error: (error) => {
+                            console.log('Error', error);
+                        }
+                    });
+                }
+            });
         }
 
         $.ajax({
@@ -181,7 +219,7 @@ $(document).ready(function(){
                 $("#department_total").html("")
                 $.each(department_total, function (key, value) {
                     $("#department_total").append(
-                        "<li class='m-3'>"+value["department"]+": <span>"+value["amount"]+"</span></li>"
+                        "<li class='m-3 department'><span class='department_item'>"+value["department"]+"</span>: <span>"+value["amount"]+"</span></li>"
                         )
                     })
 
@@ -261,16 +299,15 @@ $(document).ready(function(){
                 if (start_index == 0){
                     start_index+=per_page
                 }
-                
                 $.each(updated_data,function(key,item) {
                     item["data"]=item.data.slice(start_index,start_index+per_page)
                 });
                 
                 var values = Object.values(labels).slice(start_index, start_index + per_page);
-                
+                if (values.length>0){
                 employee_chart(updated_data,values)
                 start_index+=per_page
-
+            }
             },
             error: (error) => {
                 console.log('Error', error);
@@ -280,7 +317,6 @@ $(document).ready(function(){
     })
 
     $("#employee-previous").on("click" ,function () {
-
         var period = $("#monthYearField").val()
         $.ajax({
             url: '/payroll/dashboard-employee-chart',
@@ -294,31 +330,20 @@ $(document).ready(function(){
             },
             success: (response) => {
                 dataSet = response.dataset;
-                labels = response.labels;
-                if (start_index  <= 0)   {
-                    updated_data= dataSet
-                    $.each(updated_data,function(key,item) {
-                        item["data"]=item.data.slice(start_index,start_index+per_page,)
-                    });
-                    var values = Object.values(labels).slice(start_index, start_index + per_page);
-
-                    employee_chart(updated_data,values)
-                    start_index+=per_page
-                }
-                else {
-                    start_index -= per_page
-                    if (start_index <= 0){
-
-                    }
-                    else{
-                    updated_data= dataSet
-                    $.each(updated_data,function(key,item) {
-                        item["data"]=item.data.slice(start_index-per_page,start_index)
-                    });
-                    var values = Object.values(labels).slice(start_index - per_page, start_index );
-                    employee_chart(updated_data,values)
-                }
-                }
+            labels = response.labels;
+            
+            if (start_index <= 0) {
+                return;
+            }
+            start_index -= per_page;
+            if (start_index > 0) {
+                updated_data = dataSet.map(item => ({
+                    ...item,
+                    data: item.data.slice(start_index - per_page, start_index)
+                }));
+                var values = Object.values(labels).slice(start_index - per_page, start_index);
+                employee_chart(updated_data, values);
+            }
             },
             error: (error) => {
                 console.log('Error', error);
@@ -333,8 +358,9 @@ $(document).ready(function(){
         var start_date = $("#start_date").val()
         var end_date = $("#end_date").val()
         var employee = $("#select_employee").val()
+        var status = $("#select_status").val()
 
-        let url = "/payroll/dashboard-export/"+period+"?start_date="+start_date+"&end_date="+end_date+"&employee="+employee;
+        let url = "/payroll/dashboard-export/"+"?start_date="+start_date+"&end_date="+end_date+"&employee="+employee+"&status="+status;
         window.location.href = url;
 
     })
@@ -371,7 +397,29 @@ $(document).ready(function(){
     $("#ContractModal").on("click",".oh-modal__close",function(){
         $("#ContractModal").removeClass("oh-modal--show")
     })
+
+    $("#department_total").on("click",".department",function(){
+        department = $(this).children(".department_item").text()
+        $.ajax({
+            url: '/payroll/filter-payslip?department='+department,
+            type: "GET",
+            dataType: "html",
+            headers: {
+                "X-Requested-With": "XMLHttpRequest",
+            },
+            success: (response) => {
+                $("#back_button").removeClass("d-none")
+                $("#dashboard").html(response)
+
+
+            },
+            error: (error) => {
+                console.log('Error', error);
+            }
+        });
+    })
     
+
 
     
 
--- payroll/templatetags/yes_no.py
+++ payroll/templatetags/yes_no.py
@@ -6,3 +6,10 @@ register = template.Library()
 @register.filter(name="yes_no")
 def yesno(value):
     return "Yes" if value else "No"
+
+@register.filter(name="on_off")
+def on_off(value):
+    if value == "on":
+        return "Yes"
+    elif value == "off":
+        return "No"
