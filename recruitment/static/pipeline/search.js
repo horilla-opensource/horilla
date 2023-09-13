3483399ed5c6a608a1c19dc0ade1cb2d00b47288
Horilla <teamhorilla@gmail.com>
2023-09-13 16:55:21 +0530

    [ADD] Help text in recruitment

--- recruitment/static/pipeline/search.js
+++ recruitment/static/pipeline/search.js
@@ -1,70 +1,90 @@
+function closeTag(event) {
+  console.log(event.target);
+  $(".oh-filter-tag.filter-field").remove();
+  $("#pipelineSearch").val("");
+  $("#pipelineSearch").trigger("keyup");
+  $("#job_pos_id").val("");
+  $("#select2-job_pos_id-container").text("------------------");
+}
+
 $(document).ready(function () {
-	$("#pipelineSearch").keyup(function (e) {
-		e.preventDefault();
-		var search = $(this).val().toLowerCase();
-		$(".candidate-container div.change-cand").each(function () {
-			var candidate = $(this).attr("data-candidate");
-			if (candidate.toLowerCase().includes(search)) {
-				$(this).show();
-			} else {
-				$(this).hide();
-			}
-			let stageId = $(this).parent().attr("data-stage-id");
-			var count = $(this).parent().find(".candidate:visible").length;
-			badge = $(`#stageCount${stageId}`).html(count);
-			$(`#stageCount${stageId}`).attr("title",`${count} candidates`)
-		});
-	});
-	function count_element () {
-		let stage = $(".recruitment_items")
-		let count = $(".stage_count")
-		let nos = []
-		for(i=0;i<stage.length;i++){
-			nos.push($(stage[i]).find("[data-candidate]:visible").length)
-		}
+  $("#pipelineSearch").keyup(function (e) {
+    e.preventDefault();
+    var search = $(this).val().toLowerCase();
+    $(".candidate-container div.change-cand").each(function () {
+      var candidate = $(this).attr("data-candidate");
+      if (candidate.toLowerCase().includes(search)) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+      let stageId = $(this).parent().attr("data-stage-id");
+      var count = $(this).parent().find(".candidate:visible").length;
+      badge = $(`#stageCount${stageId}`).html(count);
+      $(`#stageCount${stageId}`).attr("title", `${count} candidates`);
+    });
+    $(".oh-filter-tag.filter-field").remove();
+    if (search != "") {
+      $(".oh-filter-tag-container.filter-value").append(
+        '<span class="oh-filter-tag filter-field" onclick="closeTag(event)">Search : ' +
+          search +
+          '<button class="oh-filter-tag__close" id="close"><ion-icon name="close-outline" role="img" class="md hydrated" aria-label="close outline"></ion-icon></button></span>'
+      );
+    }
+  });
+  function count_element() {
+    let stage = $(".recruitment_items");
+    let count = $(".stage_count");
+    let nos = [];
+    for (i = 0; i < stage.length; i++) {
+      nos.push($(stage[i]).find("[data-candidate]:visible").length);
+    }
 
-		$.each(nos, function(index, value1) {
-			var value2 = count[index];
-			console.log(value2);
-			$(value2).text(value1);
-			$(value2).attr('title',`${value1} candidates`)
-		});
-	}
+    $.each(nos, function (index, value1) {
+      var value2 = count[index];
+      console.log(value2);
+      $(value2).text(value1);
+      $(value2).attr("title", `${value1} candidates`);
+    });
+  }
 
-	function job_Position (){
-		let search = $("#job_pos_id").val().toLowerCase()
-		if (search != "") {
-			job = $("[data-job-position]:visible")
-			job.each(function () {
-				var candidate = $(this).attr("data-job-position");
-				if (candidate.toLowerCase().includes(search)) {
-					$(this).show();
-				} else {
-					$(this).hide();
-				}
-			});
-		}
+  function job_Position() {
+    let search = $("#job_pos_id").val().toLowerCase();
+    $(".oh-filter-tag.filter-field").remove();
+    if (search != "") {
+      job = $("[data-job-position]:visible");
+      job.each(function () {
+        var candidate = $(this).attr("data-job-position");
+        if (candidate.toLowerCase().includes(search)) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+      $(".oh-filter-tag-container.filter-value").append(
+        '<span class="oh-filter-tag filter-field" onclick="closeTag(event)">Job position : ' +
+          search +
+          '<button class="oh-filter-tag__close" id="close"><ion-icon name="close-outline" role="img" class="md hydrated" aria-label="close outline"></ion-icon></button></span>'
+      );
+    }
+  }
 
-	}
-
-	$("#filter_item").on("click",function(){
-		var candidate = $("[data-job-position]");
-		candidate.each(function () { 
-				$(this).show();
-		})
-		$(".pipeline_items").each(function () {
-			$(this).removeClass("d-none");
-		});
-		job_Position();
-		count_element();
-
-	})
-	$(".oh-tabs__tab").on("click",function (){
-		$(".pipeline_items").each(function () {
-			$(this).removeClass("d-none");
-		});
-		job_Position();
-		count_element();
-	})
-	
+  $("#filter_item").on("click", function () {
+    var candidate = $("[data-job-position]");
+    candidate.each(function () {
+      $(this).show();
+    });
+    $(".pipeline_items").each(function () {
+      $(this).removeClass("d-none");
+    });
+    job_Position();
+    count_element();
+  });
+  $(".oh-tabs__tab").on("click", function () {
+    $(".pipeline_items").each(function () {
+      $(this).removeClass("d-none");
+    });
+    job_Position();
+    count_element();
+  });
 });
