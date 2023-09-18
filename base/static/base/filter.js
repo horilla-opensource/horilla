5c90f52378e4f79dbdfdbb0e6fc15e588e11985f
Horilla <teamhorilla@gmail.com>
2023-09-18 10:23:22 +0530

    [ADD] Filter count

---
+++ base/static/base/filter.js
@@ -0,0 +1,23 @@
+function filterFormSubmit(formId) {
+  var formData = $("#" + formId).serialize();
+  var count = 0;
+  formData.split("&").forEach(function (field) {
+    var parts = field.split("=");
+    var value = parts[1];
+    if (value && value !== "unknown") {
+      count++;
+    }
+  });
+  $("#filterCount").empty();
+  if (count > 0) {
+    $("#filterCount").text("(" + count + ")");
+  }
+}
+
+$("#filterForm").submit(function (e) {
+  filterFormSubmit("filterForm");
+});
+
+$("#filterForm2").submit(function (e) {
+  filterFormSubmit("filterForm2");
+});
