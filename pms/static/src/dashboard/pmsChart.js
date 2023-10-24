f8e25d7459765a6a86b61addfd486c1e13abc9bd
Horilla <teamhorilla@gmail.com>
2023-10-24 19:15:46 +0530

    [UPDT] BASE: Added empty chart template

--- pms/static/src/dashboard/pmsChart.js
+++ pms/static/src/dashboard/pmsChart.js
@@ -1,268 +1,311 @@
-
-
-const objectiveChart = document.getElementById('objectiveChart');
-// data dictionary 
+const objectiveChart = document.getElementById("objectiveChart");
+// data dictionary
 var objectiveStatusData = {
-    labels: [],
-    datasets: [{
-        label: '',
-        data: [],
-        backgroundColor: [  '#8de5b3',  '#f0a8a6',  '#8ed1f7',  '#f8e08e',  '#c2c7cc'],
-        hoverOffset: 3
-    }]
+	labels: [],
+	datasets: [
+		{
+			label: "",
+			data: [],
+			backgroundColor: ["#8de5b3", "#f0a8a6", "#8ed1f7", "#f8e08e", "#c2c7cc"],
+			hoverOffset: 3,
+		},
+	],
 };
-
+function isChartEmpty(chartData) {
+    if (!chartData) {
+        return true; 
+    }
+    for (let i = 0; i < chartData.length; i++) {
+        if (chartData[i] && chartData[i].data) {
+            const hasNonZeroValues = chartData[i].data.some((value) => value !== 0);
+            if (hasNonZeroValues) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 // chart constructor
 if (objectiveChart != null) {
-    var objectiveStatusChart = new Chart(objectiveChart, {
-        type: "doughnut",
-        data: objectiveStatusData,
-        options: {
-            onClick: (e, activeEls) => {
-              let datasetIndex = activeEls[0].datasetIndex;
-              let dataIndex = activeEls[0].index;
-              let datasetLabel = e.chart.data.datasets[datasetIndex].label;
-              let value = e.chart.data.datasets[datasetIndex].data[dataIndex];
-              let label = e.chart.data.labels[dataIndex];
-              let params = "?status="+label+"&archive=false"
+	var objectiveStatusChart = new Chart(objectiveChart, {
+		type: "doughnut",
+		data: objectiveStatusData,
+		options: {
+			onClick: (e, activeEls) => {
+				let datasetIndex = activeEls[0].datasetIndex;
+				let dataIndex = activeEls[0].index;
+				let datasetLabel = e.chart.data.datasets[datasetIndex].label;
+				let value = e.chart.data.datasets[datasetIndex].data[dataIndex];
+				let label = e.chart.data.labels[dataIndex];
+				let params = "?status=" + label + "&archive=false";
 
-
-                $.ajax({
-                    url: "/pms/objective-list-search"+params,
-                    type: "GET",
-                    headers: {
-                        "X-Requested-With": "XMLHttpRequest",
-                    },
-                    success: (response) => {
-                        $("#dashboard").html(response)
-                    },
-                    error: (error) => {
-                        console.log('Error', error);
-                    }
-                })
-                $("#back_button").removeClass("d-none")
-            }
-        }
-    });
+				$.ajax({
+					url: "/pms/objective-list-search" + params,
+					type: "GET",
+					headers: {
+						"X-Requested-With": "XMLHttpRequest",
+					},
+					success: (response) => {
+						$("#dashboard").html(response);
+					},
+					error: (error) => {
+						console.log("Error", error);
+					},
+				});
+				$("#back_button").removeClass("d-none");
+			},
+		},
+	});
 }
 
-function objectiveStatusDataUpdate(data){
-    objectiveStatusData.labels = data.objective_label
-    objectiveStatusData.datasets[0].data = data.objective_value
-    objectiveStatusChart.update()
+function objectiveStatusDataUpdate(data) {
+	objectiveStatusData.labels = data.objective_label;
+	objectiveStatusData.datasets[0].data = data.objective_value;
+	objectiveStatusChart.update();
 }
 
 $.ajax({
-    url: '/pms/dashboard-objective-status',
-    type: "GET",
-    dataType: "json",
-    headers: {
-        "X-Requested-With": "XMLHttpRequest",
-    },
-    success: (response) => {
-        objectiveStatusDataUpdate(response)
-        
-    },
-    error: (error) => {
-        console.log('Error', error);
-    }
+	url: "/pms/dashboard-objective-status",
+	type: "GET",
+	dataType: "json",
+	headers: {
+		"X-Requested-With": "XMLHttpRequest",
+	},
+	success: (response) => {
+		if (isChartEmpty(response.data)) {
+			$("#objectiveChart")
+				.parent()
+				.html(
+					`<div style="height: 320px; display:flex;align-items: center;justify-content: center;" class="">
+                        <div style="" class="">
+                        <img style="display: block;width: 70px;margin: 20px auto ;" src="/static/images/ui/joiningchart.png" class="" alt=""/>
+                        <h3 style="font-size:16px" class="oh-404__subtitle">${response.message}</h3>
+                        </div>
+                    </div>`
+				);
+		} else {
+			objectiveStatusDataUpdate(response);
+		}
+	},
+	error: (error) => {
+		console.log("Error", error);
+	},
 });
 
-
 // chart change
-$('#objective-status-chart').click(function (e) { 
-    var chartType = objectiveStatusChart.config.type
-    if (chartType === 'line') {
-        chartType = 'bar';
-    } else if(chartType==='bar') {
-        chartType = 'doughnut';
-    } else if(chartType==='doughnut'){
-        chartType = 'pie'
-    }else if(chartType==='pie'){
-        chartType = 'line'
-    }
-    objectiveStatusChart.config.type = chartType;
-    objectiveStatusChart.update();
-    
-})
+$("#objective-status-chart").click(function (e) {
+	var chartType = objectiveStatusChart.config.type;
+	if (chartType === "line") {
+		chartType = "bar";
+	} else if (chartType === "bar") {
+		chartType = "doughnut";
+	} else if (chartType === "doughnut") {
+		chartType = "pie";
+	} else if (chartType === "pie") {
+		chartType = "line";
+	}
+	objectiveStatusChart.config.type = chartType;
+	objectiveStatusChart.update();
+});
 
 // objecitve chart section end
 
-const keyResultStatusChartCtx = document.getElementById('keyResultChart');
+const keyResultStatusChartCtx = document.getElementById("keyResultChart");
 
-// data dictionary 
+// data dictionary
 var keyResultStatusData = {
-    labels: [],
-    datasets: [{
-        label: '',
-        data: [],
-        backgroundColor: [  '#8de5b3',  '#f0a8a6',  '#8ed1f7',  '#f8e08e',  '#c2c7cc'],
-        hoverOffset: 3
-    }]
+	labels: [],
+	datasets: [
+		{
+			label: "",
+			data: [],
+			backgroundColor: ["#8de5b3", "#f0a8a6", "#8ed1f7", "#f8e08e", "#c2c7cc"],
+			hoverOffset: 3,
+		},
+	],
 };
 
 // chart constructor
 if (keyResultStatusChartCtx != null) {
-    var keyResultStatusChart = new Chart(keyResultStatusChartCtx, {
-        type: "pie",
-        data: keyResultStatusData,
-        options: {
-            onClick: (e, activeEls) => {
-              let datasetIndex = activeEls[0].datasetIndex;
-              let dataIndex = activeEls[0].index;
-              let datasetLabel = e.chart.data.datasets[datasetIndex].label;
-              let value = e.chart.data.datasets[datasetIndex].data[dataIndex];
-              let label = e.chart.data.labels[dataIndex];
-              let params = "?status="+label+"&archive=false"
+	var keyResultStatusChart = new Chart(keyResultStatusChartCtx, {
+		type: "pie",
+		data: keyResultStatusData,
+		options: {
+			onClick: (e, activeEls) => {
+				let datasetIndex = activeEls[0].datasetIndex;
+				let dataIndex = activeEls[0].index;
+				let datasetLabel = e.chart.data.datasets[datasetIndex].label;
+				let value = e.chart.data.datasets[datasetIndex].data[dataIndex];
+				let label = e.chart.data.labels[dataIndex];
+				let params = "?status=" + label + "&archive=false";
 
-
-                $.ajax({
-                    url: "/pms/key-result-view"+params,
-                    type: "GET",
-                    headers: {
-                        "X-Requested-With": "XMLHttpRequest",
-                    },
-                    success: (response) => {
-                        $("#dashboard").html(response)
-                    },
-                    error: (error,response) => {
-                        console.log(response)
-                        console.log('Error', error);
-                    }
-                })
-                $("#back_button").removeClass("d-none")
-            }
-        }
-    });
+				$.ajax({
+					url: "/pms/key-result-view" + params,
+					type: "GET",
+					headers: {
+						"X-Requested-With": "XMLHttpRequest",
+					},
+					success: (response) => {
+						$("#dashboard").html(response);
+					},
+					error: (error, response) => {
+						console.log(response);
+						console.log("Error", error);
+					},
+				});
+				$("#back_button").removeClass("d-none");
+			},
+		},
+	});
 }
 
-function keyResultStatusDataUpdate(data){
-    keyResultStatusData.labels = data.key_result_label
-    keyResultStatusData.datasets[0].data = data.key_result_value
-    keyResultStatusChart.update()
+function keyResultStatusDataUpdate(data) {
+	keyResultStatusData.labels = data.key_result_label;
+	keyResultStatusData.datasets[0].data = data.key_result_value;
+	keyResultStatusChart.update();
 }
 
 $.ajax({
-    url: '/pms/dashbord-key-result-status',
-    type: "GET",
-    dataType: "json",
-    headers: {
-        "X-Requested-With": "XMLHttpRequest",
-    },
-    success: (response) => {
-        keyResultStatusDataUpdate(response)
-    },
-    error: (error) => {
-        console.log('Error', error);
-    }
+	url: "/pms/dashbord-key-result-status",
+	type: "GET",
+	dataType: "json",
+	headers: {
+		"X-Requested-With": "XMLHttpRequest",
+	},
+	success: (response) => {
+		if (isChartEmpty(response.data)) {
+			$("#keyResultChart")
+				.parent()
+				.html(
+					`<div style="height: 320px; display:flex;align-items: center;justify-content: center;" class="">
+                        <div style="" class="">
+                        <img style="display: block;width: 70px;margin: 20px auto ;" src="/static/images/ui/joiningchart.png" class="" alt=""/>
+                        <h3 style="font-size:16px" class="oh-404__subtitle">${response.message}</h3>
+                        </div>
+                    </div>`
+				);
+		} else {
+			keyResultStatusDataUpdate(response);
+		}
+	},
+	error: (error) => {
+		console.log("Error", error);
+	},
 });
 
-
 // chart change
-$('#key-result-status-chart').click(function (e) { 
-    var chartType = keyResultStatusChart.config.type
-    if (chartType === 'line') {
-        chartType = 'bar';
-    } else if(chartType==='bar') {
-        chartType = 'doughnut';
-    } else if(chartType==='doughnut'){
-        chartType = 'pie'
-    }else if(chartType==='pie'){
-        chartType = 'line'
-    }
-    keyResultStatusChart.config.type = chartType;
-    keyResultStatusChart.update();
-    
-})
+$("#key-result-status-chart").click(function (e) {
+	var chartType = keyResultStatusChart.config.type;
+	if (chartType === "line") {
+		chartType = "bar";
+	} else if (chartType === "bar") {
+		chartType = "doughnut";
+	} else if (chartType === "doughnut") {
+		chartType = "pie";
+	} else if (chartType === "pie") {
+		chartType = "line";
+	}
+	keyResultStatusChart.config.type = chartType;
+	keyResultStatusChart.update();
+});
 
-// key result chart section 
+// key result chart section
 
-const feedbackStatusChartCtx = document.getElementById('feedbackChart');
+const feedbackStatusChartCtx = document.getElementById("feedbackChart");
 
-
-// data dictionary 
+// data dictionary
 var feedbackStatusData = {
-    labels: [],
-    datasets: [{
-        label: 'Feedback',
-        data: [],
-        backgroundColor: [  '#8de5b3',  '#f0a8a6',  '#8ed1f7',  '#f8e08e',  '#c2c7cc'],
-        hoverOffset: 3
-    }]
+	labels: [],
+	datasets: [
+		{
+			label: "Feedback",
+			data: [],
+			backgroundColor: ["#8de5b3", "#f0a8a6", "#8ed1f7", "#f8e08e", "#c2c7cc"],
+			hoverOffset: 3,
+		},
+	],
 };
 
 // chart constructor
 if (feedbackStatusChartCtx != null) {
-    var feedbackStatusChart = new Chart(feedbackStatusChartCtx, {
-        type: "pie",
-        data: feedbackStatusData,
-        options: {
-            onClick: (e, activeEls) => {
-              let datasetIndex = activeEls[0].datasetIndex;
-              let dataIndex = activeEls[0].index;
-              let datasetLabel = e.chart.data.datasets[datasetIndex].label;
-              let value = e.chart.data.datasets[datasetIndex].data[dataIndex];
-              let label = e.chart.data.labels[dataIndex];
-              let params = "?status="+label+"&archive=false"
+	var feedbackStatusChart = new Chart(feedbackStatusChartCtx, {
+		type: "pie",
+		data: feedbackStatusData,
+		options: {
+			onClick: (e, activeEls) => {
+				let datasetIndex = activeEls[0].datasetIndex;
+				let dataIndex = activeEls[0].index;
+				let datasetLabel = e.chart.data.datasets[datasetIndex].label;
+				let value = e.chart.data.datasets[datasetIndex].data[dataIndex];
+				let label = e.chart.data.labels[dataIndex];
+				let params = "?status=" + label + "&archive=false";
 
-
-                $.ajax({
-                    url: "/pms/feedback-list-search"+params,
-                    type: "GET",
-                    headers: {
-                        "X-Requested-With": "XMLHttpRequest",
-                    },
-                    success: (response) => {
-                        $("#dashboard").html(response)
-                    },
-                    error: (error) => {
-                        console.log('Error', error);
-                    }
-                })
-                $("#back_button").removeClass("d-none")
-            }
-        }
-    });
+				$.ajax({
+					url: "/pms/feedback-list-search" + params,
+					type: "GET",
+					headers: {
+						"X-Requested-With": "XMLHttpRequest",
+					},
+					success: (response) => {
+						$("#dashboard").html(response);
+					},
+					error: (error) => {
+						console.log("Error", error);
+					},
+				});
+				$("#back_button").removeClass("d-none");
+			},
+		},
+	});
 }
-    
-function feedbackStatusDataUpdate(data){
-    feedbackStatusData.labels = data.feedback_label
-    feedbackStatusData.datasets[0].data = data.feedback_value
-    feedbackStatusChart.update()
+
+function feedbackStatusDataUpdate(data) {
+	feedbackStatusData.labels = data.feedback_label;
+	feedbackStatusData.datasets[0].data = data.feedback_value;
+	feedbackStatusChart.update();
 }
 
 $.ajax({
-    url: '/pms/dashboard-feedback-status',
-    type: "GET",
-    dataType: "json",
-    headers: {
-        "X-Requested-With": "XMLHttpRequest",
-    },
-    success: (response) => {
-        feedbackStatusDataUpdate(response)
-    },
-    error: (error) => {
-        console.log('Error', error);
-    }
+	url: "/pms/dashboard-feedback-status",
+	type: "GET",
+	dataType: "json",
+	headers: {
+		"X-Requested-With": "XMLHttpRequest",
+	},
+	success: (response) => {
+		if (isChartEmpty(response.data)) {
+			$("#feedbackChart")
+				.parent()
+				.html(
+					`<div style="height: 320px; display:flex;align-items: center;justify-content: center;" class="">
+                        <div style="" class="">
+                        <img style="display: block;width: 70px;margin: 20px auto ;" src="/static/images/ui/joiningchart.png" class="" alt=""/>
+                        <h3 style="font-size:16px" class="oh-404__subtitle">${response.message}</h3>
+                        </div>
+                    </div>`
+				);
+		} else {
+			feedbackStatusDataUpdate(response);
+		}
+	},
+	error: (error) => {
+		console.log("Error", error);
+	},
 });
 
-
 // chart change
-$('#feedback-status-chart').click(function (e) { 
-    var chartType = feedbackStatusChart.config.type
-    if (chartType === 'line') {
-        chartType = 'bar';
-    } else if(chartType==='bar') {
-        chartType = 'doughnut';
-    } else if(chartType==='doughnut'){
-        chartType = 'pie'
-    }else if(chartType==='pie'){
-        chartType = 'line'
-    }
-    feedbackStatusChart.config.type = chartType;
-    feedbackStatusChart.update();
-    
-})
-
-
+$("#feedback-status-chart").click(function (e) {
+	var chartType = feedbackStatusChart.config.type;
+	if (chartType === "line") {
+		chartType = "bar";
+	} else if (chartType === "bar") {
+		chartType = "doughnut";
+	} else if (chartType === "doughnut") {
+		chartType = "pie";
+	} else if (chartType === "pie") {
+		chartType = "line";
+	}
+	feedbackStatusChart.config.type = chartType;
+	feedbackStatusChart.update();
+});
---
+++ pms/static/src/okr/action.js
@@ -0,0 +1,243 @@
+var archiveMessages = {
+  ar: "هل ترغب حقاً في أرشفة جميع الأهداف المحددة؟",
+  de: "Möchten Sie wirklich alle ausgewählten Ziele archivieren?",
+  es: "¿Realmente quieres archivar todos los objetivos seleccionados?",
+  en: "Do you really want to archive all the selected objectives?",
+  fr: "Voulez-vous vraiment archiver tous les objectifs sélectionnés?",
+};
+
+var unarchiveMessages = {
+  ar: "هل ترغب حقاً في إلغاء الأرشفة عن جميع الأهداف المحددة؟",
+  de: "Möchten Sie wirklich alle ausgewählten Ziele aus der Archivierung nehmen?",
+  es: "¿Realmente quieres desarchivar todos los objetivos seleccionados?",
+  en: "Do you really want to unarchive all the selected objectives?",
+  fr: "Voulez-vous vraiment désarchiver tous les objectifs sélectionnés?",
+};
+
+var deleteMessages = {
+  ar: "هل ترغب حقاً في حذف جميع الأهداف المحددة؟",
+  de: "Möchten Sie wirklich alle ausgewählten Ziele löschen?",
+  es: "¿Realmente quieres eliminar todos los objetivos seleccionados?",
+  en: "Do you really want to delete all the selected objectives?",
+  fr: "Voulez-vous vraiment supprimer tous les objectifs sélectionnés?",
+};
+
+var norowMessages = {
+  ar: "لم يتم تحديد أي صفوف.",
+  de: "Es wurden keine Zeilen ausgewählt.",
+  es: "No se han seleccionado filas.",
+  en: "No rows have been selected.",
+  fr: "Aucune ligne n'a été sélectionnée.",
+};
+
+
+$(".all-objects").change(function (e) {
+    var is_checked = $(this).is(":checked");
+    if (is_checked) {
+        $(".all-objects-row").prop("checked", true);
+    } else {
+        $(".all-objects-row").prop("checked", false);
+    }
+});
+
+$(".own-objects").change(function (e) {
+    var is_checked = $(this).is(":checked");
+    if (is_checked) {
+        $(".own-objects-row").prop("checked", true);
+    } else {
+        $(".own-objects-row").prop("checked", false);
+    }
+});
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+  
+  function getCurrentLanguageCode(callback) {
+    $.ajax({
+      type: "GET",
+      url: "/employee/get-language-code/",
+      success: function (response) {
+        var languageCode = response.language_code;
+        callback(languageCode); // Pass the language code to the callback
+      },
+    });
+  }
+
+$("#archiveObjectives").click(function (e) {
+    e.preventDefault();
+
+    var languageCode = null;
+    getCurrentLanguageCode(function (code) {
+      languageCode = code;
+      var confirmMessage = archiveMessages[languageCode];
+      var textMessage = norowMessages[languageCode];
+      var checkedRows = $(".all-objects-row").filter(":checked");
+      if (checkedRows.length === 0) {
+        var checkedRows = $(".own-objects-row").filter(":checked");
+      }
+      if (checkedRows.length === 0) {
+            Swal.fire({
+            text: textMessage,
+            icon: "warning",
+            confirmButtonText: "Close",
+            });
+        } else {
+          Swal.fire({
+            text: confirmMessage,
+            icon: "info",
+            showCancelButton: true,
+            confirmButtonColor: "#008000",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Confirm",
+            }).then(function (result) {
+            if (result.isConfirmed) {
+                e.preventDefault();
+                ids = [];
+                checkedRows.each(function () {
+                ids.push($(this).attr("id"));
+            });
+            
+            $.ajax({
+                type: "POST",
+                url: "/pms/objective-bulk-archive?is_active=False",
+                data: {
+                    csrfmiddlewaretoken: getCookie("csrftoken"),
+                ids: JSON.stringify(ids),
+                },
+                success: function (response, textStatus, jqXHR) {
+                    if (jqXHR.status === 200) {
+                        location.reload(); // Reload the current page
+                    } else {
+                        // console.log("Unexpected HTTP status:", jqXHR.status);
+                    }
+                },
+            });
+           }
+        });
+      }
+    });
+});
+
+$("#unArchiveObjectives").click(function (e) {
+    e.preventDefault();
+
+    var languageCode = null;
+    getCurrentLanguageCode(function (code) {
+      languageCode = code;
+      var confirmMessage = unarchiveMessages[languageCode];
+      var textMessage = norowMessages[languageCode];
+      var checkedRows = $(".all-objects-row").filter(":checked");
+      if (checkedRows.length === 0) {
+        var checkedRows = $(".own-objects-row").filter(":checked");
+      }
+      if (checkedRows.length === 0) {
+            Swal.fire({
+            text: textMessage,
+            icon: "warning",
+            confirmButtonText: "Close",
+            });
+        } else {
+          Swal.fire({
+            text: confirmMessage,
+            icon: "info",
+            showCancelButton: true,
+            confirmButtonColor: "#008000",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Confirm",
+            }).then(function (result) {
+            if (result.isConfirmed) {
+                e.preventDefault();
+                ids = [];
+                checkedRows.each(function () {
+                ids.push($(this).attr("id"));
+                });
+                
+                $.ajax({
+                    type: "POST",
+                    url: "/pms/objective-bulk-archive?is_active=True",
+                    data: {
+                        csrfmiddlewaretoken: getCookie("csrftoken"),
+                        ids: JSON.stringify(ids),
+                    },
+                    success: function (response, textStatus, jqXHR) {
+                        if (jqXHR.status === 200) {
+                            location.reload(); // Reload the current page
+                        } else {
+                            // console.log("Unexpected HTTP status:", jqXHR.status);
+                        }
+                    },
+                });
+            }
+        });
+        }
+    });
+});
+
+$("#deleteObjectives").click(function (e) {
+    e.preventDefault();
+
+    var languageCode = null;
+    getCurrentLanguageCode(function (code) {
+      languageCode = code;
+      var confirmMessage = deleteMessages[languageCode];
+      var textMessage = norowMessages[languageCode];
+      var checkedRows = $(".all-objects-row").filter(":checked");
+      if (checkedRows.length === 0) {
+        var checkedRows = $(".own-objects-row").filter(":checked");
+      }
+      if (checkedRows.length === 0) {
+            Swal.fire({
+            text: textMessage,
+            icon: "warning",
+            confirmButtonText: "Close",
+            });
+        } else {
+          Swal.fire({
+            text: confirmMessage,
+            icon: "error",
+            showCancelButton: true,
+            confirmButtonColor: "#008000",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Confirm",
+            }).then(function (result) {
+            if (result.isConfirmed) {
+                e.preventDefault();
+                ids = [];
+                checkedRows.each(function () {
+                ids.push($(this).attr("id"));
+                });
+                
+                $.ajax({
+                    type: "POST",
+                    url: "/pms/objective-bulk-delete",
+                    data: {
+                    csrfmiddlewaretoken: getCookie("csrftoken"),
+                    ids: JSON.stringify(ids),
+                    },
+                    success: function (response, textStatus, jqXHR) {
+                        if (jqXHR.status === 200) {
+                            location.reload(); // Reload the current page
+                        } else {
+                            // console.log("Unexpected HTTP status:", jqXHR.status);
+                        }
+                    },
+                });
+
+            }
+        });
+    }
+});
+});
