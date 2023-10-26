04780947ca6bf71cc560b1eeee1aeba567c5d12e
Horilla <teamhorilla@gmail.com>
2023-10-26 12:52:33 +0530

    [ADD] TRACKING: Template filter tag

--- base/templatetags/basefilters.py
+++ base/templatetags/basefilters.py
@@ -1,73 +1,77 @@
 from django.template.defaultfilters import register
 from django import template
 from employee.models import Employee, EmployeeWorkInformation
-from django.core.paginator import Page,Paginator
+from django.core.paginator import Page, Paginator
 
 
-
-def paginator_qry(qryset,page_number):
+def paginator_qry(qryset, page_number):
     """
-    This method is used to paginate queryset 
+    This method is used to paginate queryset
     """
-    paginator = Paginator(qryset, 50) 
+    paginator = Paginator(qryset, 50)
     qryset = paginator.get_page(page_number)
     return qryset
 
 
 register = template.Library()
-@register.filter(name='cancel_request')
-def cancel_request(user,request):
+
+
+@register.filter(name="cancel_request")
+def cancel_request(user, request):
     employee = user.employee_get
     employee_manages = employee.reporting_manager.all()
     return bool(
         request.employee_id == employee
-        or user.has_perm('perms.base.cancel_worktyperequest')
-        or user.has_perm('perms.base.cancel_shiftrequest')
+        or user.has_perm("perms.base.cancel_worktyperequest")
+        or user.has_perm("perms.base.cancel_shiftrequest")
         or employee_manages.exists()
     )
 
-@register.filter(name='update_request')
-def update_request(user,request):
+
+@register.filter(name="update_request")
+def update_request(user, request):
     employee = user.employee_get
     return bool(
         not request.canceled
         and not request.approved
         and (
             employee == request.employee_id
-            or user.has_perm('perms.base.change_worktyperequest')
-            or user.has_perm('perms.base.change_shiftrequest')
+            or user.has_perm("perms.base.change_worktyperequest")
+            or user.has_perm("perms.base.change_shiftrequest")
         )
     )
 
-@register.filter(name='is_reportingmanager')
+
+@register.filter(name="is_reportingmanager")
 def is_reportingmanager(user):
     """{% load basefilters %}
 
     This method will return true if the user employee profile is reporting manager to any employee
     """
     employee = Employee.objects.filter(employee_user_id=user).first()
-    return EmployeeWorkInformation.objects.filter(reporting_manager_id=employee).exists()
+    return EmployeeWorkInformation.objects.filter(
+        reporting_manager_id=employee
+    ).exists()
 
 
-
-@register.filter(name='filtersubordinates')
+@register.filter(name="filtersubordinates")
 def filtersubordinates(user):
     """
     This method returns true if the user employee has corresponding related reporting manager object in EmployeeWorkInformation model
     args:
         user    : request.user
     """
-    employee =user.employee_get
+    employee = user.employee_get
     employee_manages = employee.reporting_manager.all()
     return employee_manages.exists()
 
 
-@register.filter(name='filter_field')
-def filter_field(value):  
-    if value.endswith("_id"):    
-        value = value[:-3] 
+@register.filter(name="filter_field")
+def filter_field(value):
+    if value.endswith("_id"):
+        value = value[:-3]
     if value.endswith("_ids"):
-        value = value[:-4]   
+        value = value[:-4]
     splitted = value.split("__")
 
-    return splitted[-1].replace('_', ' ').capitalize()
+    return splitted[-1].replace("_", " ").capitalize()
