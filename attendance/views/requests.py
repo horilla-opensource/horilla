36f3ca4f0bb3a12bd16553b77d3f37a3c193cace
Horilla <teamhorilla@gmail.com>
2023-12-04 13:40:58 +0530

    [FIX] ATTENDANCE: Attendance request work type and shift field select issue

--- attendance/views/requests.py
+++ attendance/views/requests.py
@@ -153,8 +153,18 @@ def attendance_request_changes(request, attendance_id):
     """
     attendance = Attendance.objects.get(id=attendance_id)
     form = AttendanceRequestForm(instance=attendance)
+    form.fields["work_type_id"].widget.attrs.update({"class":"w-100","style":"height:50px;border-radius:0;border:1px solid hsl(213deg,22%,84%)"})
+    form.fields["shift_id"].widget.attrs.update({"class":"w-100","style":"height:50px;border-radius:0;border:1px solid hsl(213deg,22%,84%)"})
     if request.method == "POST":
         form = AttendanceRequestForm(request.POST, instance=copy.copy(attendance))
+        form.fields["work_type_id"].widget.attrs.update({"class":"w-100","style":"height:50px;border-radius:0;border:1px solid hsl(213deg,22%,84%)"})
+        form.fields["shift_id"].widget.attrs.update({"class":"w-100","style":"height:50px;border-radius:0;border:1px solid hsl(213deg,22%,84%)"})
+        work_type_id = form.data["work_type_id"]
+        shift_id = form.data["shift_id"]
+        if work_type_id is None or len(shift_id):
+            form.add_error("work_type_id","This field is required")
+        if shift_id is None or len(work_type_id):
+            form.add_error("shift_id","This field is required")
         if form.is_valid():
             # commit already set to False
             # so the changes not affected to the db
