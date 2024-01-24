01a78aeed35e9f91419fa21651aef2e427575682
Horilla <teamhorilla@gmail.com>
2024-01-24 12:28:16 +0530

    [UPDT] OFFBOARDING: User level updates

--- offboarding/decorators.py
+++ offboarding/decorators.py
@@ -4,24 +4,28 @@ offboarding/decorators.py
 This module is used to write custom authentication decorators for offboarding module
 """
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from horilla.decorators import decorator_with_arguments
 from offboarding.models import Offboarding, OffboardingStage, OffboardingTask
 
 
 @decorator_with_arguments
-def any_manager_can_enter(function, perm):
+def any_manager_can_enter(function, perm, offboarding_employee_can_enter=False):
     def _function(request, *args, **kwargs):
         employee = request.user.employee_get
-        if request.user.has_perm(perm) or (
-            Offboarding.objects.filter(managers=employee).exists()
-            | OffboardingStage.objects.filter(managers=employee).exists()
-            | OffboardingTask.objects.filter(managers=employee).exists()
+        if (
+            request.user.has_perm(perm)
+            or offboarding_employee_can_enter
+            or (
+                Offboarding.objects.filter(managers=employee).exists()
+                | OffboardingStage.objects.filter(managers=employee).exists()
+                | OffboardingTask.objects.filter(managers=employee).exists()
+            )
         ):
             return function(request, *args, **kwargs)
         else:
             messages.info(request, "You dont have permission.")
-            return HttpResponseRedirect(request.META.get("HTTP_REFERER", "/"))
+            return HttpResponse("<script>window.location.reload()</script>")
 
     return _function
 
@@ -37,7 +41,7 @@ def offboarding_manager_can_enter(function, perm):
             return function(request, *args, **kwargs)
         else:
             messages.info(request, "You dont have permission.")
-            return HttpResponseRedirect(request.META.get("HTTP_REFERER", "/"))
+            return HttpResponse("<script>window.location.reload()</script>")
 
     return _function
 
@@ -54,6 +58,6 @@ def offboarding_or_stage_manager_can_enter(function, perm):
             return function(request, *args, **kwargs)
         else:
             messages.info(request, "You dont have permission.")
-            return HttpResponseRedirect(request.META.get("HTTP_REFERER", "/"))
+            return HttpResponse("<script>window.location.reload()</script>")
 
     return _function
--- offboarding/templatetags/offboarding_filter.py
+++ offboarding/templatetags/offboarding_filter.py
@@ -6,10 +6,12 @@ This page is used to write custom template filters.
 
 from django.template.defaultfilters import register
 from django import template
+from employee.models import Employee
 
 from offboarding.models import (
     EmployeeTask,
     Offboarding,
+    OffboardingEmployee,
     OffboardingStage,
     OffboardingTask,
 )
@@ -19,7 +21,7 @@ register = template.Library()
 
 
 @register.filter(name="stages")
-def stages(stages_dict, stage):
+def stages(stages_dict: dict, stage: OffboardingStage):
     """
     This method will return stage drop accordingly to the offboarding
     """
@@ -32,7 +34,7 @@ def stages(stages_dict, stage):
 
 
 @register.filter(name="have_task")
-def have_task(task, employee):
+def have_task(task: OffboardingTask, employee: Employee):
     """
     used to check the task is for the employee
     """
@@ -40,7 +42,7 @@ def have_task(task, employee):
 
 
 @register.filter(name="get_assigned_task")
-def get_assigned_tak(employee, task):
+def get_assigned_tak(employee: Employee, task: OffboardingTask):
     """
     This method is used to filterout the assigned task
     """
@@ -51,7 +53,7 @@ def get_assigned_tak(employee, task):
 
 
 @register.filter(name="any_manager")
-def any_manager(employee):
+def any_manager(employee: Employee):
     """
     This method is used to check the employee is in managers
     employee: Employee model instance
@@ -64,8 +66,58 @@ def any_manager(employee):
 
 
 @register.filter(name="is_offboarding_manager")
-def is_offboarding_manager(employee):
+def is_offboarding_manager(employee: Employee):
     """
     This method is used to check the employee is manager of any offboarding
     """
     return Offboarding.objects.filter(managers=employee).exists()
+
+
+@register.filter(name="is_offboarding_employee")
+def is_offboarding_employee(employee: Employee):
+    """
+    This method is used to check the employee is in offboarding employee
+    """
+    return OffboardingEmployee.objects.filter(employee_id=employee).exists()
+
+
+@register.filter("is_in_managers")
+def is_in_managers(employee: Employee, instance: object):
+    """
+    This method is used to check the employee in the managers or not
+    """
+    is_in_managers = False
+    if isinstance(instance, (Offboarding, OffboardingStage)):
+        # checking in offboarding managers
+        is_in_managers = instance.managers.filter(employee_id=employee).exists()
+    if isinstance(instance, OffboardingEmployee):
+        # also checking in the offboarding employee
+        is_in_managers = is_in_managers | (employee == instance.employee_id)
+    return is_in_managers
+
+
+@register.filter("is_in_offboarding")
+def is_in_offboarding(employee: Employee, offboarding: Offboarding):
+    """
+    This method is used to check the employee in the offboarding or not
+    """
+    return (
+        (employee in offboarding.managers.all())
+        or OffboardingStage.objects.filter(
+            offboarding_id=offboarding, managers=employee
+        ).exists()
+        or OffboardingEmployee.objects.filter(
+            stage_id__offboarding_id=offboarding, employee_id=employee
+        ).exists()
+    )
+
+
+@register.filter("is_any_stage_manager")
+def is_any_stage_manager(employee):
+    """
+    This method is used to to check any stage manager
+    """
+    return (
+        OffboardingStage.objects.filter(managers=employee).exists()
+        | Offboarding.objects.filter(managers=employee).exists()
+    )
