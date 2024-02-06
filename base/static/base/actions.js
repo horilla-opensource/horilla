e3474231c452e2b73b010af1cdeee426d6c13f70
Horilla <teamhorilla@gmail.com>
2024-02-06 12:51:50 +0530

    [UPDT] BASE: Base getCurrentLanguageCode function to check and retrieve language code from HTML attribute

--- base/static/base/actions.js
+++ base/static/base/actions.js
@@ -102,15 +102,29 @@ function getCookie(name) {
   return cookieValue;
 }
 
-function getCurrentLanguageCode(callback) {
-  $.ajax({
-    type: "GET",
-    url: "/employee/get-language-code/",
-    success: function (response) {
-      var languageCode = response.language_code;
-      callback(languageCode); // Pass the language code to the callback
-    },
-  });
+function getCurrentLanguageCode(callback) {  
+  var languageCode = $("#main-section-data").attr("data-lang");
+  var allowedLanguageCodes = ["ar", "de", "es", "en", "fr"];
+  if (allowedLanguageCodes.includes(languageCode)) {
+    callback(languageCode);
+  } else {
+    $.ajax({
+      type: "GET",
+      url: "/employee/get-language-code/",
+      success: function (response) {
+        var ajaxLanguageCode = response.language_code;
+        $("#main-section-data").attr("data-lang", ajaxLanguageCode);
+        callback(
+          allowedLanguageCodes.includes(ajaxLanguageCode)
+            ? ajaxLanguageCode
+            : "en"
+        );
+      },
+      error: function () {
+        callback("en");
+      },
+    });
+  }
 }
 
 $(".all-rshift").change(function (e) {
