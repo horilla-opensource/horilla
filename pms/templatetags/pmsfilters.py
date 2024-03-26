34ed3e82654f7d2af5ad3b86ce8a179665385c33
Horilla <teamhorilla@gmail.com>
2024-03-26 15:02:28 +0530

    [UPDT] PMS: Employee objectives permission updation

--- pms/templatetags/pmsfilters.py
+++ pms/templatetags/pmsfilters.py
@@ -1,6 +1,7 @@
 from django.template.defaultfilters import register
 
-from pms.models import Objective
+from employee.models import Employee
+from pms.models import EmployeeObjective, Objective
 
 
 @register.filter(name="replace")
@@ -21,3 +22,24 @@ def kr_count(objective_id):
         for kr in obj.employee_key_result.all():
             kr_list.append(kr)
     return kr_list
+
+@register.filter(name="is_manager_or_owner")
+def is_manager_or_owner(objective,user):
+    """
+    This method will return true, if the user is manger of the objective, or owner
+    """
+    employee = Employee.objects.filter(employee_user_id=user).first()
+    if (EmployeeObjective.objects.filter(id=objective.id, objective_id__managers=employee).exists() or
+        EmployeeObjective.objects.filter(id=objective.id, employee_id=employee).exists()):
+        return True
+    return False
+
+@register.filter(name="is_manager")
+def is_manager(objective,user):
+    """
+    This method will return true, if the user is manger of the objective, or owner
+    """
+    employee = Employee.objects.filter(employee_user_id=user).first()
+    if EmployeeObjective.objects.filter(id=objective.id, objective_id__managers=employee).exists():
+        return True
+    return False
