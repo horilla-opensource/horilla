e02778a5f6c0023ec87743164433521a9c2f0f18
Ankit Tiwari <67471937+ankit25821@users.noreply.github.com>
2024-04-24 15:17:34 +0530

    [fix] Bug fix for collectstatic #156 #157 (#158)

--- horilla/settings.py
+++ horilla/settings.py
@@ -170,13 +170,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
