fe92fcdb7a909f08d9396a55203365f629db5f13
Horilla <teamhorilla@gmail.com>
2024-06-20 11:49:11 +0530

    [UPDT] ASSET: Update asset app by changing related name of employee in AssetAssignment model class

--- asset/models.py
+++ asset/models.py
@@ -91,12 +91,25 @@ class Asset(HorillaModel):
 
 
 class AssetReport(HorillaModel):
+    """
+    Model representing a report for an asset.
+
+    Attributes:
+    - title: A CharField for the title of the report (optional).
+    - asset_id: A ForeignKey to the Asset model, linking the report to a specific asset.
+    """
+
     title = models.CharField(max_length=255, blank=True, null=True)
     asset_id = models.ForeignKey(
         Asset, related_name="asset_report", on_delete=models.CASCADE
     )
 
     def __str__(self):
+        """
+        Returns a string representation of the AssetReport instance.
+        If a title is present, it returns "asset_id - title".
+        Otherwise, it returns "report for asset_id".
+        """
         return (
             f"{self.asset_id} - {self.title}"
             if self.title
@@ -105,18 +118,35 @@ class AssetReport(HorillaModel):
 
 
 class AssetDocuments(HorillaModel):
+    """
+    Model representing documents associated with an asset report.
+
+    Attributes:
+    - asset_report: A ForeignKey to the AssetReport model, linking the document to
+    a specific asset report.
+    - file: A FileField for uploading the document file (optional).
+    """
+
     asset_report = models.ForeignKey(
         "AssetReport", related_name="documents", on_delete=models.CASCADE
     )
     file = models.FileField(
         upload_to="asset/asset_report/documents/", blank=True, null=True
     )
+    objects = models.Manager()
 
     def __str__(self):
         return f"document for {self.asset_report}"
 
 
 class ReturnImages(HorillaModel):
+    """
+    Model representing images associated with a returned asset.
+
+    Attributes:
+    - image: A FileField for uploading the image file (optional).
+    """
+
     image = models.FileField(upload_to="asset/return_images/", blank=True, null=True)
 
 
@@ -134,7 +164,7 @@ class AssetAssignment(HorillaModel):
         Asset, on_delete=models.PROTECT, verbose_name=_("asset")
     )
     assigned_to_employee_id = models.ForeignKey(
-        Employee, on_delete=models.PROTECT, related_name="allocated_employeee"
+        Employee, on_delete=models.PROTECT, related_name="allocated_employee"
     )
     assigned_date = models.DateField(auto_now_add=True)
     assigned_by_employee_id = models.ForeignKey(
@@ -155,6 +185,8 @@ class AssetAssignment(HorillaModel):
     )
 
     class Meta:
+        """Meta class for AssetAssignment model"""
+
         ordering = ["-id"]
 
     def __str__(self):
@@ -189,4 +221,6 @@ class AssetRequest(HorillaModel):
     )
 
     class Meta:
+        """Meta class for AssetRequest model"""
+
         ordering = ["-id"]
