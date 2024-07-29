2581d2732bebfbfe46a275ca6bae6cfcd48074ea
Horilla <teamhorilla@gmail.com>
2024-07-29 16:50:00 +0530

    [FIX] HORILLA VIEWS: Active view type in Horilla nav

--- horilla_views/admin.py
+++ horilla_views/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
-from horilla_views.models import ActiveGroup, ActiveTab, ToggleColumn
+from horilla_views import models
 
-admin.site.register([ToggleColumn, ActiveTab, ActiveGroup])
+admin.site.register(
+    [
+        models.ToggleColumn,
+        models.ActiveTab,
+        models.ActiveGroup,
+        models.SavedFilter,
+        models.ActiveView,
+    ]
+)
--- horilla_views/models.py
+++ horilla_views/models.py
@@ -50,3 +50,38 @@ class ActiveGroup(HorillaModel):
     path = models.CharField(max_length=256)
     group_target = models.CharField(max_length=256)
     group_by_field = models.CharField(max_length=256)
+
+
+class SavedFilter(HorillaModel):
+    """
+    SavedFilter
+    """
+
+    title = models.CharField(max_length=20, null=True)
+    color = models.CharField(max_length=10, default="")
+    is_default = models.BooleanField(default=False)
+    filter = models.TextField()
+    urlencode = models.TextField(default="")
+    path = models.CharField(max_length=256)
+    referrer = models.CharField(max_length=256, default="")
+
+    def save(self, *args, **kwargs):
+        SavedFilter.objects.filter(
+            is_default=True, path=self.path, created_by=self.created_by
+        ).exclude(id=self.pk).update(is_default=False)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+
+class ActiveView(HorillaModel):
+    """
+    This model to store the active view type for HNV
+    """
+
+    path = models.CharField(max_length=256)
+    type = models.CharField(max_length=50)
+
+    def save(self, *args, **kwargs):
+        return super().save(*args, **kwargs)
