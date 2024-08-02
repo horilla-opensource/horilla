974f6f680591ef255a13a750eb2b81a6bbbb4655
Horilla <teamhorilla@gmail.com>
2024-08-02 14:27:23 +0530

    [UPDT] BASE: Added history for models using django audit log

--- horilla_audit/methods.py
+++ horilla_audit/methods.py
@@ -5,7 +5,11 @@ This module is used to write methods related to the history
 """
 
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from django.db import models
+from django.shortcuts import render
+
+from horilla.decorators import apply_decorators
 
 
 class Bot:
@@ -146,3 +150,27 @@ def get_diff(instance):
             if track_fields:
                 delta_changes = filter_history(delta_changes, track_fields)
     return delta_changes
+
+
+def history_tracking(request, obj_id, **kwargs):
+    model = kwargs.get("model")
+    decorator_strings = kwargs.get("decorators", [])
+
+    @apply_decorators(decorator_strings)
+    def _history_tracking(request, obj_id, model):
+        instance = model.objects.get(pk=obj_id)
+        histories = instance.horilla_history.all()
+        page_number = request.GET.get("page", 1)
+        paginator = Paginator(histories, 4)
+        page_obj = paginator.get_page(page_number)
+        context = {
+            "histories": page_obj,
+            "model_name": model,
+        }
+        return render(
+            request,
+            "horilla_audit/history_tracking.html",
+            context,
+        )
+
+    return _history_tracking(request, obj_id, model)
--- horilla_audit/templatetags/audit_filters.py
+++ horilla_audit/templatetags/audit_filters.py
@@ -1,8 +1,4 @@
-from django import template
-from django.core.paginator import Page, Paginator
 from django.template.defaultfilters import register
-
-from employee.models import Employee, EmployeeWorkInformation
 
 
 @register.filter(name="fk_history")
@@ -18,3 +14,19 @@ def fk_history(instance, change):
         value = str(value) + f" (Previous {change['field']} deleted)"
         pass
     return value
+
+
+@register.filter(name="verbose_name")
+def verbose_name(model, field_name=None):
+    """ "
+    This method is used to fine the verbose name of a field
+    """
+    if not field_name:
+        model_name = model._meta.verbose_name.capitalize()
+        return model_name
+
+    try:
+        field = model._meta.get_field(field_name)
+        return field.verbose_name
+    except:
+        return field_name
