2a632fb1ee3cf29ac2d5925e0774782b708f77cd
Horilla <teamhorilla@gmail.com>
2024-08-03 16:14:08 +0530

    [IMP] OFFBOARDING: Offboarding module dependency removal from other Horilla apps

--- offboarding/apps.py
+++ offboarding/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class OffboardingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "offboarding"
+
+    def ready(self):
+        from django.urls import include, path
+
+        from horilla.urls import urlpatterns
+
+        urlpatterns.append(
+            path("offboarding/", include("offboarding.urls")),
+        )
+        super().ready()
