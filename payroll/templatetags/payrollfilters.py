358a1581f0f00e6e46c656b654abacc00318439b
Horilla <teamhorilla@gmail.com>
2024-08-22 17:36:01 +0530

    [UPDT] PAYROLL: Added total, paid and balance amount to installment view

--- payroll/templatetags/payrollfilters.py
+++ payroll/templatetags/payrollfilters.py
@@ -3,7 +3,16 @@ from django import template
 register = template.Library()
 
 
+@register.filter(name="paid_amount")
+def paid_amount(installment):
+    paid = [
+        deduction.amount for deduction in installment if deduction.installment_payslip()
+    ]
+
+    return sum(paid)
+
+
 @register.filter(name="balance_amount")
 def balance_amount(amount, installment):
-    balance = amount - sum(installment.values_list("amount", flat=True))
+    balance = amount - paid_amount(installment)
     return balance
