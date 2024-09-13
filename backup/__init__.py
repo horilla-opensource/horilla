03e81e54b7a5fb08ae67f6a05e9b715b8aabe6a8
Horilla <teamhorilla@gmail.com>
2024-09-13 18:00:38 +0530

    [ADD] HORILLA DB BACKUP: Add horilla database backup module

---
+++ backup/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'backup.apps.backupConfig'
---
+++ backup/migrations/__init__.py
@@ -0,0 +1,15 @@
+import atexit
+
+
+def shutdown_function():
+    from backup.models import GoogleDriveBackup, LocalBackup
+    if GoogleDriveBackup.objects.exists():
+        google_drive_backup = GoogleDriveBackup.objects.first()
+        google_drive_backup.active  = False
+        google_drive_backup.save()
+    if LocalBackup.objects.exists():
+        local_backup = LocalBackup.objects.first()
+        local_backup.active = False
+        local_backup.save()
+
+atexit.register(shutdown_function)
