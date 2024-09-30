22920420580413453d8cc02491fe95c351ce2faf
Horilla <support@horilla.com>
2024-09-30 14:28:32 +0530

    [FIX] HORILLA BACKUP: Form script error

--- horilla_backup/apps.py
+++ horilla_backup/apps.py
@@ -1,5 +1,6 @@
-from django.apps import AppConfig        
+from django.apps import AppConfig
+
 
 class BackupConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'horilla_backup'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "horilla_backup"
