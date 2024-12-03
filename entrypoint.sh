2a48df659537b785fb221595a9d491979383700b
rell3k <keller.jeff@gmail.com>
2024-12-03 08:52:34 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 python3 manage.py createhorillauser --first_name admin --last_name admin --username admin --password admin --email admin@example.com --phone 1234567890
-gunicorn --bind 0.0.0.0:8000 horilla.wsgi:application
+gunicorn --bind 0.0.0.0:8080 horilla.wsgi:application
