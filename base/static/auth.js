055ab6e9eb4f474c9f5f8fb17b6266d8daf9317e
Viktor <bozhinoski.v@gmail.com>
2024-12-11 19:23:33 +0100

    Add Javascript for manipulating the input and input elements

---
+++ base/static/auth.js
@@ -0,0 +1,68 @@
+const chipInput = document.querySelector('.chip-input');
+const chipsContainer = document.querySelector('.chips');
+const chipInputContainer = document.querySelector('.chip-input-container');
+const invalidDomainMessage = document.querySelector('.invalid-domain');
+const domainSaveButton = document.querySelector('.domain-save-btn');
+const form = document.getElementById('domain-form');
+const domainRegex = /^[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+let domains = [];
+
+
+const isDomainValid = ((inputText) => domainRegex.test(inputText));
+
+const processInput = ((inputText) => {
+  if (inputText) createChip(inputText, isDomainValid(inputText));
+});
+
+const setChipInputState = ((inputText, chip, isValid) => {
+  if (isValid) {
+    chip.style.backgroundColor = 'green';
+    domains.push(inputText);
+  } else {
+    chip.style.backgroundColor = 'red';
+    invalidDomainMessage.style.display = 'block';
+    chipInput.setAttribute("readonly", true);
+    domainSaveButton.disabled = true;
+  }
+});
+
+const createChip = ((inputText, isValid) => {
+  const chip = document.createElement('div');
+  chip.className = 'chip';
+  chip.innerHTML = `
+        <span>${inputText}</span>
+        <span class="chip-remove">&times;</span>
+    `;
+
+  setChipInputState(inputText, chip, isValid);
+
+  chipsContainer.appendChild(chip);
+
+  chip.querySelector('.chip-remove').addEventListener('click', () => {
+    chipsContainer.removeChild(chip);
+    if (!isValid) {
+      invalidDomainMessage.style.display = 'none';
+      domainSaveButton.disabled = false;
+      chipInput.removeAttribute("readonly");
+      chipInput.style.visibility = 'visible';
+      chipInput.value = '';
+    }
+    domains = domains.filter(item => item === inputText ? false : true);
+  });
+  chipInput.value = '';
+});
+
+chipInput.addEventListener('keydown', (event) => {
+  if (event.key === ' ' || event.key === 'Enter') {
+    event.preventDefault();
+    processInput(chipInput.value.trim());
+  }
+});
+
+domainSaveButton.addEventListener('click', () => {
+  processInput(chipInput.value.trim());
+  chipInput.value = domains.join(',');
+  chipInput.style.visibility = 'hidden';
+});
+
+chipInputContainer.addEventListener('click', () => chipInput.focus());
