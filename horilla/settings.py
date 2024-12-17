0914cd8c7bf2f46291e7038510aaf1642aa13878
Horilla <131998600+horilla-opensource@users.noreply.github.com>
2024-12-17 15:06:30 +0530

    [RMV] SETTINGS: Remove duplicate middleware

--- horilla/settings.py
+++ horilla/settings.py
@@ -88,7 +88,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "base.middleware.ForcePasswordChangeMiddleware",
 ]
 
 ROOT_URLCONF = "horilla.urls"
