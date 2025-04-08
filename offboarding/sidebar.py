454314e5e912495380fff52a22c5327e0f78128c
Horilla <support@horilla.com>
2025-04-08 12:09:07 +0530

    [ADD] OFFOARDING: Added dashboard for offboarding module

--- offboarding/sidebar.py
+++ offboarding/sidebar.py
@@ -18,6 +18,11 @@ ACCESSIBILITY = "offboarding.sidebar.offboarding_accessibility"
 
 SUBMENUS = [
     {
+        "menu": _("Dashboard"),
+        "redirect": reverse("offboarding-dashboard"),
+        "accessibility": "offboarding.sidebar.dashboard_accessibility",
+    },
+    {
         "menu": _("Exit Process"),
         "redirect": reverse("offboarding-pipeline"),
     },
@@ -33,7 +38,7 @@ def offboarding_accessibility(request, menu, user_perms, *args, **kwargs):
     accessible = False
     try:
         accessible = (
-            request.user.has_perm("offboarding.view_offboarding")
+            request.user.has_module_perms("offboarding")
             or any_manager(request.user.employee_get)
             or is_offboarding_employee(request.user.employee_get)
         )
@@ -45,3 +50,13 @@ def resignation_letter_accessibility(request, menu, user_perms, *args, **kwargs)
     return resignation_request_enabled(request)[
         "enabled_resignation_request"
     ] and request.user.has_perm("offboarding.view_resignationletter")
+
+
+def dashboard_accessibility(request, *args):
+    """
+    Check if the user has permission to view the dashboard.
+    """
+
+    return request.user.has_module_perms("offboarding") or any_manager(
+        request.user.employee_get
+    )
