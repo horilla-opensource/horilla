683a9a39e4aea0617c4f82cbd20a674a7e50e2b6
Horilla <support@horilla.com>
2025-08-26 17:15:58 +0530

    [UPDT] HORILLA: Remove xss_exempt_fields from HorillaMailTemplate model.Function now reliably detects and filters malicious input

--- recruitment/models.py
+++ recruitment/models.py
@@ -242,7 +242,6 @@ class Recruitment(HorillaModel):
         help_text=_("Resume not mandatory for candidate creation"),
         verbose_name=_("Optional Resume"),
     )
-    xss_exempt_fields = ["description"]  # 807
 
     class Meta:
         """
