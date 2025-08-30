b28b2a0b609b0e91f37a597b53ddcb2e577ab2df
Horilla <support@horilla.com>
2025-08-30 12:56:08 +0530

    [UPDT] HORILLA_THEME: Updated my leave request detailed view to cbv

--- horilla_theme/overrides.py
+++ horilla_theme/overrides.py
@@ -4,17 +4,34 @@ from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
 
+class DummyModel:
+    """A dummy fallback class that behaves like a model placeholder."""
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __getattr__(self, name):
+        # if you try to access any attribute, just return another dummy
+        return DummyModel()
+
+    def __call__(self, *args, **kwargs):
+        # acts callable (like a model manager/queryset)
+        return DummyModel()
+
+
 def get_horilla_model_class(app_label, model):
     try:
         return apps.get_model(app_label, model)
     except LookupError:
-        return None
+        return DummyModel
 
 
 Recruitment = get_horilla_model_class(app_label="recruitment", model="recruitment")
 AttendanceLateComeEarlyOut = get_horilla_model_class(
     app_label="attendance", model="attendancelatecomeearlyOut"
 )
+Attendance = get_horilla_model_class(app_label="attendance", model="attendance")
+LeaveRequest = get_horilla_model_class(app_label="leave", model="leaverequest")
 
 
 def tot_hires(self):
@@ -81,8 +98,124 @@ def penalities_column(self):
         )
 
 
+def rejected_action(self):
+    if self.reject_reason:
+        if self.status == "cancelled":
+            label = _("Reason for Cancellation")
+        else:
+            label = _("Reason for Rejection")
+        return format_html(
+            """
+                <div class="w-full p-4 rounded-lg bg-orange-100/20 border border-orange-300 rounded-md">
+                    <div>
+                        <span class="block text-xs font-medium text-gray-700 mb-1">{}</span>
+                        <div class="text-sm text-gray-800 italic">{}</div>
+                    </div>
+                </div>
+            """,
+            label,
+            self.reject_reason,
+        )
+    return ""
+
+
+def attachment_action(self):
+    if self.attachment:
+        return format_html(
+            """
+            <a href="{}" target="_blank" class="w-50 bg-gray-100 p-4 flex items-center text-gray-700 text-sm font-medium">
+                <ion-icon name="download-outline" class="me-1 text-lg"></ion-icon>
+                <span class="ml-1">{}</span>
+            </a>
+            """,
+            self.attachment.url,
+            _("View attachment"),
+        )
+    return ""
+
+
+def attendance_detail_activity_col(self):
+    activity_count = self.activities().get("count", 0)
+
+    if activity_count == 0:
+        return ""
+
+    label = _("Activities:")
+    view_label = _("View Activities")
+    count_label = _("Activity") if activity_count == 1 else _("Activities")
+
+    url = reverse("get-attendance-activities", args=[self.id])
+
+    col = format_html(
+        """
+        <div class="mb-2 flex gap-5 items-center">
+            <span class="font-medium text-xs text-[#565E6C] w-32">
+                {}
+            </span>
+            <p class="text-xs font-semibold flex items-center gap-5">
+                : <span>
+                    <button
+                        data-target="#activityViewModal"
+                        data-toggle="oh-modal-toggle"
+                        hx-get="{}"
+                        hx-target="#activityViewModalBody"
+                        title="{}"
+                        class="flex items-center text-primary-600 text-sm font-semibold transition-colors"
+                    >
+                        <span>{} {}</span>
+                        <ion-icon name="eye-outline" class="text-lg ml-2 mt-[2px]"></ion-icon>
+                    </button>
+                </span>
+            </p>
+        </div>
+        """,
+        label,
+        url,
+        view_label,
+        activity_count,
+        count_label,
+    )
+
+    return col
+
+
+def leave_clash_col(self):
+    count = self.leave_clashes_count
+
+    label = _("View Clashes")
+    url = reverse("view-clashes", args=[self.id])
+
+    col = format_html(
+        """
+            <div onclick="event.stopPropagation();">
+                <div class="flex "
+                    data-target="#clashModal"
+                    data-toggle="oh-modal-toggle"
+                    hx-get="{}"
+                    hx-target="#clashModalBody"
+                    title="{}">
+
+                    <i class="material-icons text-4xl" >groups</i>
+                    <span class="w-5 h-5 bg-[#e54f38] rounded-full text-white text-xs font-semibold flex items-center justify-center">{}</span>
+                </div>
+            </div>
+        """,
+        url,
+        label,
+        count,
+    )
+
+    return col
+
+
 Recruitment.managers_detail = managers_detail
 Recruitment.open_job_detail = open_job_detail
 Recruitment.tot_hires = tot_hires
 
 AttendanceLateComeEarlyOut.penalities_column = penalities_column
+Attendance.attendance_detail_activity_col = attendance_detail_activity_col
+
+LeaveRequest.rejected_action = rejected_action
+LeaveRequest.attachment_action = attachment_action
+LeaveRequest.penality_col = penalities_column
+LeaveRequest.leave_clash_col = leave_clash_col
