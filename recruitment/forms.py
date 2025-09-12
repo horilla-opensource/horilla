b95d7e076ee40a7a83a9581ac681f9551ec95a26
Horilla <support@horilla.com>
2025-09-12 11:58:14 +0530

    [UPDT] EMPLOYEE: Updated document tab and form in employee and candidate

--- recruitment/forms.py
+++ recruitment/forms.py
@@ -70,71 +70,117 @@ logger = logging.getLogger(__name__)
 
 class ModelForm(forms.ModelForm):
     """
-    Overriding django default model form to apply some styles
+    Override of Django ModelForm to add initial styling and defaults.
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        request = getattr(horilla_middlewares._thread_locals, "request", None)
+
         reload_queryset(self.fields)
+
+        request = getattr(horilla_middlewares._thread_locals, "request", None)
+
+        today = date.today()
+        now = datetime.now()
+
+        default_input_class = "oh-input w-100"
+        select_class = "oh-select oh-select-2 select2-hidden-accessible"
+        checkbox_class = "oh-switch__checkbox"
+
         for field_name, field in self.fields.items():
             widget = field.widget
-            if isinstance(widget, (forms.DateInput)):
-                field.initial = date.today()
+            label = _(field.label) if field.label else ""
 
-            if isinstance(
-                widget,
-                (forms.NumberInput, forms.EmailInput, forms.TextInput, forms.FileInput),
-            ):
-                label = _(field.label)
-                field.widget.attrs.update(
-                    {"class": "oh-input w-100", "placeholder": label}
-                )
-            elif isinstance(widget, forms.URLInput):
-                field.widget.attrs.update(
-                    {"class": "oh-input w-100", "placeholder": field.label}
-                )
-            elif isinstance(widget, (forms.Select,)):
-                field.empty_label = _("---Choose {label}---").format(
-                    label=_(field.label)
-                )
-                self.fields[field_name].widget.attrs.update(
+            # Date field
+            if isinstance(widget, forms.DateInput):
+                field.initial = today
+                widget.input_type = "date"
+                widget.format = "%Y-%m-%d"
+                field.input_formats = ["%Y-%m-%d"]
+
+                existing_class = widget.attrs.get("class", default_input_class)
+                widget.attrs.update(
                     {
-                        "id": uuid.uuid4,
-                        "class": "oh-select oh-select-2 w-100",
-                        "style": "height:50px;",
+                        "class": f"{existing_class} form-control",
+                        "placeholder": label,
                     }
                 )
-            elif isinstance(widget, (forms.Textarea)):
-                label = _(field.label)
-                field.widget.attrs.update(
+
+            # Time field
+            elif isinstance(widget, forms.TimeInput):
+                field.initial = now
+                widget.input_type = "time"
+                widget.format = "%H:%M"
+                field.input_formats = ["%H:%M"]
+
+                existing_class = widget.attrs.get("class", default_input_class)
+                widget.attrs.update(
                     {
-                        "class": "oh-input w-100",
+                        "class": f"{existing_class} form-control",
+                        "placeholder": label,
+                    }
+                )
+
+            # Number, Email, Text, File, URL fields
+            elif isinstance(
+                widget,
+                (
+                    forms.NumberInput,
+                    forms.EmailInput,
+                    forms.TextInput,
+                    forms.FileInput,
+                    forms.URLInput,
+                ),
+            ):
+                existing_class = widget.attrs.get("class", default_input_class)
+                widget.attrs.update(
+                    {
+                        "class": f"{existing_class} form-control",
+                        "placeholder": _(field.label.title()) if field.label else "",
+                    }
+                )
+
+            # Select fields
+            elif isinstance(widget, forms.Select):
+                if not isinstance(field, forms.ModelMultipleChoiceField):
+                    field.empty_label = _("---Choose {label}---").format(label=label)
+                existing_class = widget.attrs.get("class", select_class)
+                widget.attrs.update({"class": existing_class})
+
+            # Textarea
+            elif isinstance(widget, forms.Textarea):
+                existing_class = widget.attrs.get("class", default_input_class)
+                widget.attrs.update(
+                    {
+                        "class": f"{existing_class} form-control",
                         "placeholder": label,
                         "rows": 2,
                         "cols": 40,
                     }
                 )
+
+            # Checkbox types
             elif isinstance(
-                widget,
-                (
-                    forms.CheckboxInput,
-                    forms.CheckboxSelectMultiple,
-                ),
+                widget, (forms.CheckboxInput, forms.CheckboxSelectMultiple)
             ):
-                field.widget.attrs.update({"class": "oh-switch__checkbox "})
+                existing_class = widget.attrs.get("class", checkbox_class)
+                widget.attrs.update({"class": existing_class})
 
-            try:
-                self.fields["employee_id"].initial = request.user.employee_get
-            except:
-                pass
+        # Set employee_id and company_id once
+        if request:
+            employee = getattr(request.user, "employee_get", None)
+            if employee:
+                if "employee_id" in self.fields:
+                    self.fields["employee_id"].initial = employee
 
-            try:
-                self.fields["company_id"].initial = (
-                    request.user.employee_get.get_company
-                )
-            except:
-                pass
+                if "company_id" in self.fields:
+                    company_field = self.fields["company_id"]
+                    company = getattr(employee, "get_company", None)
+                    if company:
+                        queryset = company_field.queryset
+                        company_field.initial = (
+                            company if company in queryset else queryset.first()
+                        )
 
 
 class RegistrationForm(forms.ModelForm):
@@ -1366,7 +1412,7 @@ class CandidateDocumentForm(ModelForm):
         Render the form fields as HTML table rows with Bootstrap styling.
         """
         context = {"form": self}
-        table_html = render_to_string("common_form.html", context)
+        table_html = render_to_string("horilla_form.html", context)
         return table_html
 
 
