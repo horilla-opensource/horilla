2ab869d7a47e2db9b38f7c10586aba98683cb755
Horilla <support@horilla.com>
2025-10-22 15:13:26 +0530

    [UPDT] ATTENDANCE: Formatted check in checkout in settings page and removed toggle form

--- attendance/cbv/check_in_check_out.py
+++ attendance/cbv/check_in_check_out.py
@@ -1,6 +1,5 @@
 from typing import Any
 
-from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from attendance.filters import AttendanceGeneralSettingFilter
@@ -19,17 +18,12 @@ class CheckInCheckOutListView(HorillaListView):
 
     model = AttendanceGeneralSetting
     filter_class = AttendanceGeneralSettingFilter
+    show_toggle_form = False
 
     columns = [
         (_("Company"), "company_col"),
         (_("Check in/Check out"), "check_in_check_out_col"),
     ]
-
-    header_attrs = {
-        "company_col": """
-                   style = "width:100px !important"
-                   """,
-    }
 
     bulk_select_option = False
 
@@ -38,10 +32,6 @@ class CheckInCheckOutNavBar(HorillaNavView):
     """
     Nav bar
     """
-
-    def __init__(self, **kwargs: Any) -> None:
-        super().__init__(**kwargs)
-        # self.search_url = reverse("check-in-check-out-list")
 
     nav_title = _("Enable Check In/Check out")
     filter_instance = AttendanceGeneralSettingFilter()
