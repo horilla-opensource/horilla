a5be3ffb8b6c55e056199f45b121c1047f7cbd90
I.A.B.P Rathnayake <90783913+BhathiyaPrasad@users.noreply.github.com>
2025-12-04 18:34:46 +0530

    Add duplicate attendance record warning in Attendance form

--- attendance/views/views.py
+++ attendance/views/views.py
@@ -2822,8 +2822,31 @@ def check_compensation(request):
 
     form = AttendanceForm()
 
+    show_duplicate_warning = check_attendance_duplicate(request)
+
     return render(
         request,
         "attendance/attendance/_compensation_field.html",
-        {"show_field": result["is_mercantile_holiday"], "form": form},
+        {"show_field": result["is_mercantile_holiday"], "form": form , "show_duplicate_warning": show_duplicate_warning,},
     )
+
+def check_attendance_duplicate(request):
+    print("request triggered")
+    employee_id = request.GET.get("employee_id")
+    attendance_date = request.GET.get("attendance_date")
+
+    if not employee_id or not attendance_date:
+        print("no emlpoyee id or date")
+        return HttpResponse("")
+
+    exists = Attendance.objects.filter(
+        employee_id=employee_id,
+        attendance_date=attendance_date
+    ).exists()
+
+    if exists:
+        return exists
+
+    return HttpResponse("")
+
+
