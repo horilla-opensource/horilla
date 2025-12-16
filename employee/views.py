9584b2ffbabf7b6b8c519da27e4487bb268854a8
Horilla <support@horilla.com>
2025-12-16 12:14:43 +0530

    [FIX] EMPLOYEE: Fixed Employee profile image to only accept image files

--- employee/views.py
+++ employee/views.py
@@ -1552,7 +1552,7 @@ def update_profile_image(request, obj_id):
         employee.save()
         messages.success(request, _("Profile image updated."))
     except Exception:
-        messages.error(request, _("No image chosen."))
+        messages.error(request, _("Upload a valid image."))
     response = render(
         request,
         "employee/profile/profile_modal.html",
@@ -1568,11 +1568,14 @@ def update_own_profile_image(request):
     """
     This method is used to update own profile image from profile view form
     """
-    employee = request.user.employee_get
-    img = request.FILES.get("employee_profile")
-    employee.employee_profile = img
-    employee.save()
-    messages.success(request, _("Profile image updated."))
+    try:
+        employee = request.user.employee_get
+        img = request.FILES.get("employee_profile")
+        employee.employee_profile = img
+        employee.save()
+        messages.success(request, _("Profile image updated."))
+    except Exception:
+        messages.error(request, _("Upload a valid image."))
     response = render(
         request,
         "employee/profile/profile_modal.html",
@@ -1655,7 +1658,7 @@ def employee_create_update_personal_info(request, obj_id=None):
     This method is used to update employee's personal info.
     """
     employee = Employee.objects.filter(id=obj_id).first()
-    form = EmployeeForm(request.POST, instance=employee)
+    form = EmployeeForm(request.POST, request.FILES, instance=employee)
     if form.is_valid():
         form.save()
         if obj_id is None:
