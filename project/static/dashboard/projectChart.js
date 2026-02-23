49efdccf4ff2616f21b97077328291fd7d8fedff
Horilla <support@horilla.com>
2026-02-23 13:35:22 +0530

    [UPDT] PROJECT: Update dashboard charts for dark mode

--- project/static/dashboard/projectChart.js
+++ project/static/dashboard/projectChart.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    const themedOptions = ChartTheme.getThemedOptions();
+
     function projectStatusChart(dataSet, labels) {
         const data = {
             labels: labels,
@@ -12,10 +14,23 @@ $(document).ready(function(){
             type: 'bar',
             data: data,
             options: {
+                scales: {
+                    x: {
+                        ...themedOptions.scales.x,
+                    },
+                    y: {
+                        ...themedOptions.scales.y,
+                    },
+                },
+                plugins: {
+                    ...themedOptions.plugins,
+                },
             },
         });
+        window["projectChart"] = projectChart
+        ChartTheme.observe("projectChart")
     }
-        $.ajax({
+    $.ajax({
         url: "/project/project-status-chart",
         type: "GET",
         success: function (response) {
@@ -26,64 +41,76 @@ $(document).ready(function(){
             projectStatusChart(dataSet, labels);
 
         },
-        });
-        $('#projectStatusForward').click(function (e) {
+    });
+    $('#projectStatusForward').click(function (e) {
         var chartType = projectChart.config.type
         if (chartType === 'line') {
             chartType = 'bar';
-        } else if(chartType==='bar') {
+        } else if (chartType === 'bar') {
             chartType = 'doughnut';
-        } else if(chartType==='doughnut'){
+        } else if (chartType === 'doughnut') {
             chartType = 'pie'
-        }else if(chartType==='pie'){
+        } else if (chartType === 'pie') {
             chartType = 'line'
         }
         projectChart.config.type = chartType;
         projectChart.update();
-        });
+    });
 
-        // for creating task status chart
-        function taskStatusChart(dataSet, labels) {
-            const data = {
-                labels: labels,
-                datasets: dataSet,
-            };
-            // Create chart using the Chart.js library
-            window['taskChart'] = {}
-            const ctx = document.getElementById("taskStatusCanvas").getContext("2d");
+    // for creating task status chart
+    function taskStatusChart(dataSet, labels) {
+        const data = {
+            labels: labels,
+            datasets: dataSet,
+        };
+        // Create chart using the Chart.js library
+        window['taskChart'] = {}
+        const ctx = document.getElementById("taskStatusCanvas").getContext("2d");
 
-            taskChart = new Chart(ctx, {
-                type: 'bar',
-                data: data,
-                options: {
+        taskChart = new Chart(ctx, {
+            type: 'bar',
+            data: data,
+            options: {
+                scales: {
+                    x: {
+                        ...themedOptions.scales.x,
+                    },
+                    y: {
+                        ...themedOptions.scales.y,
+                    },
                 },
-            });
-        }
-            $.ajax({
-            url: "/project/task-status-chart",
-            type: "GET",
-            success: function (response) {
-                // Code to handle the response
-                // response =  {'dataSet': [{'label': 'Odoo developer 2023-03-30', 'data': [3, 0, 5, 3]}, {'label': 'React developer 2023-03-31', 'data': [0, 1, 1, 0]}, {'label': 'Content Writer 2023-04-01', 'data': [1, 0, 0, 0]}], 'labels': ['Initial', 'Test', 'Interview', 'Hired']}
-                dataSet = response.dataSet;
-                labels = response.labels;
-                taskStatusChart(dataSet, labels);
-
+                plugins: {
+                    ...themedOptions.plugins,
+                },
             },
-            });
-            $('#taskStatusForward').click(function (e) {
-            var chartType = taskChart.config.type
-            if (chartType === 'line') {
-                chartType = 'bar';
-            } else if(chartType==='bar') {
-                chartType = 'doughnut';
-            } else if(chartType==='doughnut'){
-                chartType = 'pie'
-            }else if(chartType==='pie'){
-                chartType = 'line'
-            }
-            taskChart.config.type = chartType;
-            taskChart.update();
-            });
+        });
+        window["taskChart"] = taskChart
+        ChartTheme.observe("taskChart")
+    }
+    $.ajax({
+        url: "/project/task-status-chart",
+        type: "GET",
+        success: function (response) {
+            // Code to handle the response
+            dataSet = response.dataSet;
+            labels = response.labels;
+            taskStatusChart(dataSet, labels);
+
+        },
+    });
+    $('#taskStatusForward').click(function (e) {
+        var chartType = taskChart.config.type
+        if (chartType === 'line') {
+            chartType = 'bar';
+        } else if (chartType === 'bar') {
+            chartType = 'doughnut';
+        } else if (chartType === 'doughnut') {
+            chartType = 'pie'
+        } else if (chartType === 'pie') {
+            chartType = 'line'
+        }
+        taskChart.config.type = chartType;
+        taskChart.update();
+    });
 
 });
