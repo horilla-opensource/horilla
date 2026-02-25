99408d016a4530641ff18af08d857320b238caa4
Horilla <support@horilla.com>
2026-02-25 13:04:08 +0530

    [UPDT] PMS: Centralize permission handling in pms methods

--- pms/methods.py
+++ pms/methods.py
@@ -1,9 +1,5 @@
-from django.contrib import messages
-from django.http import HttpResponse
-from django.shortcuts import render
-from pyexpat.errors import messages
-
 from employee.models import EmployeeWorkInformation
+from horilla.methods import handle_no_permission
 from pms.models import AnonymousFeedback, EmployeeObjective, Objective
 
 decorator_with_arguments = (
@@ -29,14 +25,8 @@ def pms_manager_can_enter(function, perm):
         is_objective_manager = Objective.objects.filter(managers=employee).exists()
         if user.has_perm(perm) or is_manager or is_objective_manager:
             return function(request, *args, **kwargs)
-        else:
-            messages.info(request, "You dont have permission.")
-            previous_url = request.META.get("HTTP_REFERER", "/")
-            script = f'<script>window.location.href = "{previous_url}"</script>'
-            key = "HTTP_HX_REQUEST"
-            if key in request.META.keys():
-                return render(request, "decorator_404.html")
-            return HttpResponse(script)
+
+        return handle_no_permission(request)
 
     return _function
 
@@ -65,14 +55,8 @@ def pms_owner_and_manager_can_enter(function, perm):
             or is_objective_owner
         ):
             return function(request, *args, **kwargs)
-        else:
-            messages.info(request, "You dont have permission.")
-            previous_url = request.META.get("HTTP_REFERER", "/")
-            script = f'<script>window.location.href = "{previous_url}"</script>'
-            key = "HTTP_HX_REQUEST"
-            if key in request.META.keys():
-                return render(request, "decorator_404.html")
-            return HttpResponse(script)
+
+        return handle_no_permission(request)
 
     return _function
 
