a00d5b6c17e139db2139f0b50e7d089cc9da06cd
Horilla <support@horilla.com>
2026-03-31 11:44:42 +0530

    [RMV] HORILLA_MEET: Removed unused code

--- horilla_meet/signals.py
+++ horilla_meet/signals.py
@@ -35,23 +35,3 @@ def handle_google_meeting_save(sender, instance, created, **kwargs):
     GoogleMeeting.objects.filter(id=instance.id).update(
         meet_url=meet_link, event_id=event_id
     )
-
-
-if apps.is_installed("recruitment"):
-    from recruitment.models import InterviewSchedule
-
-    # @receiver(post_save, sender=InterviewSchedule)
-    def create_meeting_for_interview(sender, instance, created, **kwargs):
-
-        instance.refresh_from_db()
-        title = f"Interview for {instance.candidate_id}"
-        description = instance.description
-        start_time = datetime.combine(instance.interview_date, instance.interview_time)
-        print(start_time)
-        attendees = [employee.get_mail() for employee in instance.employee_id.all()]
-        attendees.append(instance.candidate_id.get_email())
-        print(attendees)
-        if instance.create_meeting:
-            print(instance)
-            print(instance.interview_date)
-            print(instance.interview_time)
