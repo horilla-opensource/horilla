8fe8ad04f50e01fd76ac006889b936f6c0e504a6
EugenePulup <eugene@ceek.com>
2026-04-13 15:09:31 +0300

    test

--- horilla/storage_backends.py
+++ horilla/storage_backends.py
@@ -1,20 +1,17 @@
-# from django.db import models
+from django.db import models
 from storages.backends.s3boto3 import S3Boto3Storage
-
-from horilla import settings
-
 
 class PrivateMediaStorage(S3Boto3Storage):
     """
     PrivateMediaStorage
     """
 
-    location = settings.env("NAMESPACE", default="private")
+    location = "private"
     default_acl = "private"
     file_overwrite = False
     custom_domain = False
 
 
 # To set the private storage globally
-# models.FileField.storage = PrivateMediaStorage()
-# models.ImageField.storage = PrivateMediaStorage()
+models.FileField.storage = PrivateMediaStorage()
+models.ImageField.storage = PrivateMediaStorage()
