9267f6896065e5fa2bf4e5d79e84b8c8cbeb994e
EugenePulup <eugene@ceek.com>
2026-04-13 15:14:50 +0300

    test new builds script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
-
-python manage.py migrate
-python manage.py collectstatic --noinput
+pip install -r requirements.txt 
+python manage.py check
+python manage.py makemigrations 
+python manage.py migrate 
+python manage.py collectstatic --noinput 
 python manage.py compilemessages
