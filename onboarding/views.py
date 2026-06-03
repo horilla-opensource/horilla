e20e2a39219f94e516636c67a4cc518485ebec03
Horilla <support@horilla.com>
2026-06-03 08:47:59 +0530

    [FIX] ONBOARDING: Add @login_required + @permission_required for offer_letter_bulk_status_update, onboarding_candidate_bulk_delete views

--- onboarding/views.py
+++ onboarding/views.py
@@ -2175,6 +2175,8 @@ def candidate_select_filter(request):
         )
 
 
+@login_required
+@permission_required("recruitment.change_candidate")
 def offer_letter_bulk_status_update(request):
     """
     This function is used to bulk update the offerletter status
@@ -2202,6 +2204,8 @@ def offer_letter_bulk_status_update(request):
     return JsonResponse("success", safe=False)
 
 
+@login_required
+@permission_required("recruitment.delete_candidate")
 def onboarding_candidate_bulk_delete(request):
     """
     This function is used to bulk delete onboarding candidates
