519cefbc3513767e9d5a00c0b7197b87af50ecc1
Horilla <support@horilla.com>
2026-07-07 14:32:48 +0530

    [FIX] SETTINGS: Update breadcrumbs on HTMX settings navigation and fix permission page swap errors

--- horilla_crumbs/context_processors.py
+++ horilla_crumbs/context_processors.py
@@ -111,6 +111,7 @@ sidebar_urls = [
     "mail-server-conf",
     "mail-templates-view",
     "multiple-approval-condition",
+    "multiple-approval-rules-view",
     "skill-zone-view",
     "view-mail-templates",
     "view-loan",
