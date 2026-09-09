8e6bf6f6772d8c28a8cb489958f007f990fac3d6
Horilla <support@horilla.com>
2026-09-09 10:25:28 +0530

    [UPDT] BASE: Added i18n context processor in settings

--- horilla/settings/base.py
+++ horilla/settings/base.py
@@ -323,6 +323,7 @@ TEMPLATES = [
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+                "django.template.context_processors.i18n",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 # Horilla dynamic context processors
